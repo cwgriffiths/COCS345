2bb4e584f848184d70020d1d1679055982f5491b
cwgriffiths <grico363@student.otago.ac.nz>
2022-07-31 13:53:10 +1200

    added menu bar

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.menu
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.materialToolbar))
+        val toolbar = findViewById<Toolbar>(R.id.materialToolbar)
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
 }
