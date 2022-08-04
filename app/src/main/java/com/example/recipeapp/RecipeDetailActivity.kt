994bdc9aa0a17858ae4f2438d4ec5f4c4d455e45
cwgriffiths <grico363@student.otago.ac.nz>
2022-08-05 11:40:50 +1200

    Improved design of recipe detail page

--- app/src/main/java/com/example/recipeapp/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/recipeapp/RecipeDetailActivity.kt
@@ -1,8 +1,12 @@
 package com.example.recipeapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
+import android.text.*
+import android.text.style.BulletSpan
+import android.view.Menu
+import androidx.core.text.toSpannable
 import com.example.recipeapp.databinding.ActivityRecipeDetailBinding
 import com.example.recipeapp.entities.RecipeEnt
 
@@ -17,33 +21,50 @@ class RecipeDetailActivity() : AppCompatActivity() {
         recipe = intent.getParcelableExtra("recipe")!!
         binding = ActivityRecipeDetailBinding.inflate(layoutInflater)
 
-        setContentView(binding.root)
+        val toolbar = binding.materialToolbar
+        toolbar.title = recipe.name
+        setSupportActionBar(toolbar)
 
-        binding.titleTxt.text = recipe.name
-        binding.descriptionTxt.text = recipe.description
-        binding.ingredientsTxt.text = commaStringToList(recipe.ingredients)
-        binding.methodTxt.text = commaStringToList(recipe.method)
-        binding.servesTxt.text = "Serves ${recipe.servings}"
+        setContentView(binding.root)
+        populateView()
+
+
     }
 
     /**
      * Take a comma separated string and return a string formatted with bullet points and new lines
      */
-    private fun commaStringToList(s: String): String{
+    private fun commaStringToList(s: String): Spannable {
         var st = s.replace(".", ",")
-        val builder = StringBuilder()
+        val builder = SpannableStringBuilder()
         val ingredients = st.split(",")
         for (ingredient in ingredients){
-            builder.append("• \t\t${ingredient.trim()}\n")
+            if(ingredient.trim().isNotEmpty()) {
+                builder.append(
+                    "${ingredient.trim()}\n",
+                    BulletSpan(40, Color.DKGRAY, 10),
+                    SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
         }
-        return builder.toString()
+        return builder.toSpannable()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     /**
      * Populate the view with the recipe data
      */
     private fun populateView() {
-
+        binding.titleTxt.text = recipe.name
+        binding.descriptionTxt.text = recipe.description
+        binding.ingredientsTxt.text = commaStringToList(recipe.ingredients)
+        binding.methodTxt.text = commaStringToList(recipe.method)
+        binding.servesTxt.text = "Serves ${recipe.servings}"
     }
 
 }
