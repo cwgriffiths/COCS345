ee11619023cb37b8687e70a3f6487dd48e228cf1
Cordell O'Leary <oleco300@student.otago.ac.nz>
2022-08-12 13:07:57 +1200

    Fixed layout error

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
