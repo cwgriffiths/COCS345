8810105bddee60fe12fc3fab402a13ddbbf95dcc
conor <grico363@student.otago.ac.nz>
2022-08-14 15:46:59 +1200

    started on filter screen

---
+++ app/src/main/java/com/example/recipeapp/fragments/RegionSelect.kt
@@ -0,0 +1,30 @@
+package com.example.recipeapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegionSelect.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegionSelect : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_region_select, container, false)
+    }
+
+}
