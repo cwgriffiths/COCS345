ba73e53d3c8df682ae840eede28bd0db588aadda
arianavl <arianavanlith@gmail.com>
2022-09-15 11:46:36 +1200

    Finished meal planner popup dialog
    Popup dialog is not currently doing anything apart from printing info to console

--- app/src/main/java/com/example/recipeapp/fragments/AddMealPlannerDialog.kt
+++ app/src/main/java/com/example/recipeapp/fragments/AddMealPlannerDialog.kt
@@ -12,43 +12,19 @@ import java.lang.IllegalStateException
 class AddMealPlannerDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-            val checkedItemsDay = ArrayList<String>()
-            val checkedItemsMeal = ArrayList<String>()
             val alertBuilder1 = AlertDialog.Builder(it)
-            val alertBuilder2 = AlertDialog.Builder(it)
-            var cont = false
+            var checkedItemDay = 0
 
             alertBuilder1.setTitle("Select a day")
-            alertBuilder1.setMultiChoiceItems(R.array.day, null, DialogInterface.OnMultiChoiceClickListener { dialog, index, checked ->
-                if (checked) {
-                    checkedItemsDay.add(index.toString())
-                } else if (checkedItemsDay.contains(index.toString())) {
-                    checkedItemsDay.remove(index.toString())
-                }
+            alertBuilder1.setSingleChoiceItems(R.array.day, 0, DialogInterface.OnClickListener { _, index ->
+                checkedItemDay = index
             })
-            alertBuilder1.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, id -> cont = true})
-            alertBuilder1.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id -> Log.d("DialogLog", "Cancelled") })
-
-
-            if (cont) {
-                Log.d("DialogLog", "Cancelled")
-                alertBuilder2.setTitle("Select a meal")
-                alertBuilder2.setMultiChoiceItems(R.array.meal,
-                    null,
-                    DialogInterface.OnMultiChoiceClickListener { dialog, index, checked ->
-                        if (checked) {
-                            checkedItemsMeal.add(index.toString())
-                        } else if (checkedItemsMeal.contains(index.toString())) {
-                            checkedItemsMeal.remove(index.toString())
-                        }
-                    })
-                alertBuilder2.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, id -> Log.d("DialogLog", "Ok pressed with checked meal: " + checkedItemsMeal + " Ok pressed with checked day: " + checkedItemsDay) })
+            alertBuilder1.setPositiveButton("OK", DialogInterface.OnClickListener { _, _ -> Log.d("DialogLog",
+                "Ok $checkedItemDay")})
+            alertBuilder1.setNegativeButton("Cancel", DialogInterface.OnClickListener { _, _ -> Log.d("DialogLog", "Cancelled") })
 
 
 
-
-                alertBuilder2.create()
-            }
 
             alertBuilder1.create()
 
