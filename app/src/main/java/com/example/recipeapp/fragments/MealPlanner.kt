fd72ac22f77f8b3115ded9b95692e1d8eb505c5d
arianavl <arianavanlith@gmail.com>
2022-09-16 14:02:38 +1200

    Changed meal planner to only having dinners

--- app/src/main/java/com/example/recipeapp/fragments/MealPlanner.kt
+++ app/src/main/java/com/example/recipeapp/fragments/MealPlanner.kt
@@ -43,51 +43,25 @@ class MealPlanner:Fragment(R.layout.fragment_meal_planner), View.OnClickListener
 
         binding = FragmentMealPlannerBinding.inflate(layoutInflater)
 
-        binding.lunchMon.setOnClickListener(this)
-        var id:Int = mealList[0].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchMon.text = recipe.name
-
         binding.dinnerMon.setOnClickListener(this)
-        id = mealList[0].dinner_recipe
+        var id = mealList[0].dinner_recipe
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerMon.text = recipe.name
-
-
-        binding.lunchTue.setOnClickListener(this)
-        id = mealList[1].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchTue.text = recipe.name
 
         binding.dinnerTue.setOnClickListener(this)
         id = mealList[1].dinner_recipe
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerTue.text = recipe.name
 
-        binding.lunchWed.setOnClickListener(this)
-        id = mealList[2].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchWed.text = recipe.name
-
         binding.dinnerWed.setOnClickListener(this)
         id = mealList[2].dinner_recipe
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerWed.text = recipe.name
 
-        binding.lunchThu.setOnClickListener(this)
-        id = mealList[3].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchThu.text = recipe.name
-
         binding.dinnerThu.setOnClickListener(this)
         id = mealList[3].dinner_recipe
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerThu.text = recipe.name
-
-        binding.lunchFri.setOnClickListener(this)
-        id = mealList[4].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchFri.text = recipe.name
 
 
         binding.dinnerFri.setOnClickListener(this)
@@ -95,20 +69,10 @@ class MealPlanner:Fragment(R.layout.fragment_meal_planner), View.OnClickListener
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerFri.text = recipe.name
 
-        binding.lunchSat.setOnClickListener(this)
-        id = mealList[5].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchSat.text = recipe.name
-
         binding.dinnerSat.setOnClickListener(this)
         id = mealList[5].dinner_recipe
         recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
         binding.dinnerSat.text = recipe.name
-
-        binding.lunchSun.setOnClickListener(this)
-        id = mealList[6].lunch_recipe
-        recipe = AppDB.getInstance(view.context).recipeDAO().getRecipeById(id)
-        binding.lunchSun.text = recipe.name
 
         binding.dinnerSun.setOnClickListener(this)
         id = mealList[6].dinner_recipe
@@ -123,62 +87,26 @@ class MealPlanner:Fragment(R.layout.fragment_meal_planner), View.OnClickListener
      * @param p0 the view
      */
     override fun onClick(p0: View) {
-        var typeMeal = 0
         val mealId = when(p0.id){
-            R.id.lunchMon -> {
-                typeMeal = 0
-                0
-            }
             R.id.dinnerMon -> {
-                typeMeal = 1
                 0
-            }
-            R.id.lunchTue -> {
-                typeMeal = 0
-                1
             }
             R.id.dinnerTue -> {
-                typeMeal = 1
                 1
             }
-            R.id.lunchWed -> {
-                typeMeal = 0
-                2
-            }
             R.id.dinnerWed -> {
-                typeMeal = 1
                 2
-            }
-            R.id.lunchThu -> {
-                typeMeal = 0
-                3
             }
             R.id.dinnerThu -> {
-                typeMeal = 1
                 3
             }
-            R.id.lunchFri -> {
-                typeMeal = 0
-                4
-            }
             R.id.dinnerFri -> {
-                typeMeal = 1
                 4
-            }
-            R.id.lunchSat -> {
-                typeMeal = 0
-                5
             }
             R.id.dinnerSat -> {
-                typeMeal = 1
                 5
             }
-            R.id.lunchSun -> {
-                typeMeal = 0
-                6
-            }
             R.id.dinnerSun -> {
-                typeMeal = 1
                 6
             }
             else -> {
@@ -189,11 +117,8 @@ class MealPlanner:Fragment(R.layout.fragment_meal_planner), View.OnClickListener
         val meal = mealList[mealId]
 
 
-        val id = if (typeMeal == 0){
-            meal.lunch_recipe
-        } else {
-            meal.dinner_recipe
-        }
+        val id = meal.dinner_recipe
+
 
         recipe = AppDB.getInstance(view2.context).recipeDAO().getRecipeById(id)
         val intent = Intent (context, RecipeDetailActivity()::class.java)
