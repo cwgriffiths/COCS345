e77b4ead6c6e631927fb274c784f89e5ed0d6e3c
arianavl <arianavanlith@gmail.com>
2022-09-20 16:01:52 +1200

    Meal planner functionailty done

--- app/src/main/java/com/example/recipeapp/fragments/AddMealPlannerDialog.kt
+++ app/src/main/java/com/example/recipeapp/fragments/AddMealPlannerDialog.kt
@@ -1,26 +1,29 @@
 package com.example.recipeapp.fragments
 
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.example.recipeapp.AppDB
 import com.example.recipeapp.R
+import com.example.recipeapp.entities.RecipeEnt
 import java.lang.IllegalStateException
 
-class AddMealPlannerDialog : DialogFragment() {
+class AddMealPlannerDialog(val recipe :RecipeEnt, private val applicationContext: Context) : DialogFragment() {
+    private lateinit var db : AppDB
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
+
             val alertBuilder1 = AlertDialog.Builder(it)
             var checkedItemDay = 0
-
             alertBuilder1.setTitle("Select a day")
             alertBuilder1.setSingleChoiceItems(R.array.day, 0, DialogInterface.OnClickListener { _, index ->
                 checkedItemDay = index
             })
-            alertBuilder1.setPositiveButton("OK", DialogInterface.OnClickListener { _, _ -> Log.d("DialogLog",
-                "Ok $checkedItemDay")})
+            alertBuilder1.setPositiveButton("OK", DialogInterface.OnClickListener { _, _ -> addMeal(checkedItemDay, recipe)})
             alertBuilder1.setNegativeButton("Cancel", DialogInterface.OnClickListener { _, _ -> Log.d("DialogLog", "Cancelled") })
 
 
@@ -29,5 +32,11 @@ class AddMealPlannerDialog : DialogFragment() {
             alertBuilder1.create()
 
         } ?: throw IllegalStateException("Exception !! Activity is null !!")
+    }
+
+    private fun addMeal(index:Int, recipe: RecipeEnt) {
+        AppDB.getInstance(applicationContext).mealPlannerDAO().addToMealPlanner(index, recipe.id)
+        Log.d("DialogLog",
+            "Ok $index recipe id ${recipe.id}")
     }
 }
