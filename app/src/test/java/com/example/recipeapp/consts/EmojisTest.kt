5c5e5dc29f71009c777c34b22b0fcad914db0384
Cordell O'Leary <cordelloleary@gmail.com>
2022-09-26 19:45:10 +1300

    added documentation

--- app/src/test/java/com/example/recipeapp/consts/EmojisTest.kt
+++ app/src/test/java/com/example/recipeapp/consts/EmojisTest.kt
@@ -3,7 +3,13 @@ package com.example.recipeapp.consts
 import org.junit.Assert.*
 import org.junit.Test
 
+/**
+ * Test for the emoji class
+ * */
 class EmojisTest{
+    /**
+     * Tests the getemoji return the right emoji or a default emoji
+     * */
     @Test
     fun getEmoji(){
         assertEquals(Emojis.getEmoji("south korea"),"\uD83C\uDDF0\uD83C\uDDF7")
