2e941a810998c4ee29d8d30cbd535d5541e53eef
Cordell O'Leary <cordelloleary@gmail.com>
2022-09-26 20:07:06 +1300

    test

--- app/src/androidTest/java/com/example/recipeapp/ui/recipe/RecipeViewModelTest.kt
+++ app/src/androidTest/java/com/example/recipeapp/ui/recipe/RecipeViewModelTest.kt
@@ -37,8 +37,8 @@ class RecipeViewModelTest : TestCase() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         val db = Room.inMemoryDatabaseBuilder(context, AppDB::class.java).allowMainThreadQueries().build()
         val dao = db.recipeDAO()
+        dao.insertRecipe(testRecipe)
         viewModel = RecipeViewModel(RecipeRepo(dao))
-        viewModel.insertRecipe(testRecipe)
     }
 
     /**
